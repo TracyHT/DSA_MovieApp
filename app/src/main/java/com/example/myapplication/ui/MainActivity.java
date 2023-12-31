1ebe37c52f56b1dbb512ed331c3b1300c439f760
Tracy <131429059+TracyHT@users.noreply.github.com>
2024-01-01 02:12:47 +0700

    Add the bottom navbar to activity_main

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.myapplication.ui;
 
 import android.os.Bundle;
-
-import com.example.myapplication.R;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.myapplication.ui.movies.MoviesFragment;
+import com.example.myapplication.ui.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +16,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Load the MoviesFragment
-        loadMoviesFragment();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_home) {
+                loadHomeFragment();
+                return true;
+            } else if (itemId == R.id.navigation_movies) {
+                loadMoviesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_ticket) {
+                loadTicketFragment();
+                return true;
+            } else if (itemId == R.id.navigation_profile) {
+                loadProfileFragment();
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
+    private void loadHomeFragment() {
+        // Implement the logic to load the HomeFragment here
+    }
+
+    private void loadTicketFragment() {
+        // Implement the logic to load the TicketFragment here
+    }
+
+    private void loadProfileFragment() {
+        ProfileFragment profileFragment = new ProfileFragment();
+
+        // Use a FragmentManager to handle the fragment transactions
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Replace the existing fragment (if any) with the ProfileFragment
+        transaction.replace(R.id.fragment_container, profileFragment);
+
+        // Commit the transaction
+        transaction.commit();
     }
 
     private void loadMoviesFragment() {
