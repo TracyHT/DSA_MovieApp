84fa0ea985376d09409dc70aaf1c838bfca87dd2
thang_iamyname <thassng2222@gmail.com>
2024-01-02 04:41:24 +0700

    Add the bottom navbar to activity_main (#10)

---
+++ app/src/main/java/com/example/myapplication/database/user/User.java
@@ -0,0 +1,58 @@
+package com.example.myapplication.database.user;
+
+public class User {
+    private int userid;
+    private String email;
+    private String phone;
+    private String username;
+    private String password;
+
+    public User(int userid, String email, String phone, String username, String password) {
+        this.userid = userid;
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/register/RegisterActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication.ui.register;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+import com.example.myapplication.database.user.User;
+import com.example.myapplication.database.user.UserDatabase;
+import com.example.myapplication.ui.login.LoginActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText userEdt, passEdt, emailEdt, phoneEdt;
+    private Button registerBtn;
+    private UserDatabase userDatabase;
+
+    private int countUser = 0;
+
+    private User newUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initView();
+    }
+
+    private void initView() {
+        emailEdt = findViewById(R.id.editTextEmailAddress);
+        phoneEdt = findViewById(R.id.editTextPhone);
+        userEdt = findViewById(R.id.registerText);
+        passEdt = findViewById(R.id.registerPassword);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countUser++;
+                if (userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()
+                        || emailEdt.getText().toString().isEmpty() || phoneEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please fill your email, phone, username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    newUser = new User(countUser, emailEdt.getText().toString(), phoneEdt.getText().toString(),
+                            userEdt.getText().toString(), passEdt.getText().toString());
+                    userDatabase.getInstance().addUser(newUser);
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                }
+            }
+        });
+    }
+}
